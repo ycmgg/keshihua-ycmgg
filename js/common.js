30c055c151361b9b61f4fb9d3edc8ad48c2257c7
ycmgg <110039709+ycmgg@users.noreply.github.com>
2022-09-14 10:38:07 +0800

    请提示封装完成

--- js/common.js
+++ js/common.js
@@ -5,3 +5,17 @@ document.addEventListener('DOMContentLoaded', () => {
     document.body.classList.add('ready')
   }, 200)
 })
+
+
+// 请提示封装函数
+const toastBox = document.querySelector('#myToast')
+const toast = new bootstrap.Toast(toastBox, {
+  animation: true,
+  autohide: true,
+  delay: 3000
+})
+
+function tip(text) {
+toastBox.querySelector('.toast-body').innerHTML = text
+toast.show()
+}
